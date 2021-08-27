9ad603978b34ad250c444db41f9f7c43cd81d9aa
simon <simon@dancingcloudservices.com>
2021-08-27 07:34:50 -0600

    Initial

---
+++ src/main/java/verify/Verify.java
@@ -0,0 +1,10 @@
+package verify;
+
+public record Verify() {
+  public static void main(String[] args) {
+    Object o = "Hello World!";
+    if (o instanceof String s) {
+      System.out.println(s);
+    }
+  }
+}
